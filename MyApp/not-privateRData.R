bf9ec506be2893f7c0b4562b759d56fe7a270fcf
Justin Garofoli <justin.garofoli@gmail.com>
2015-02-22 11:55:20 -0800

    default, empty, magic file

---
+++ MyApp/not-privateRData.R
@@ -0,0 +1,14 @@
+
+cat(" you must fill this in not-privateRdata.R ")
+
+if (username == "somename1") {
+    api_key             <- ""
+    api_secret          <- ""
+    access_token        <- ""
+    access_token_secret <- ""
+} else if (username == "somename2") {
+    api_key             <- ""
+    api_secret          <- ""
+    access_token        <- ""
+    access_token_secret <- ""
+}
